7555fe296c3100d3de4a6b314e2eeff4980b5fbd
Franklin177 <33990304+Franklin177@users.noreply.github.com>
2021-01-18 22:39:22 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -4,56 +4,51 @@ import tensorflow as tf
 
 # Verwendung von NumPy, um ein 2D-Array rechteckiger Ausschnitt von [-2,2] bis [-2,2] zu erstellen,
 # welcher mit 800/800 Pixel abgetstet wird.
-x, y = np.mgrid[-2:2:0.005, -2:2:0.005]
+y, x = np.mgrid[-2:2:0.5, -2:2:0.5]
 
 #Definieren der Koodinaten für jedes pixel
-z = x+1j*y
+zArray = x+1j*y
 
-
-c = tf.constant(z,"complex64")
-z = tf.Variable(c)
+#Erstellung von Tensoren.
+C = tf.constant(zArray)
+Z = tf.Variable(tf.zeros_like(C))
 i = tf.Variable(0)
+color = tf.Variable(tf.zeros_like(C, "int32"))
+lastMask = tf.Variable(tf.zeros_like(tf.cast(C, "bool")))
 
-lastMask = tf.Variable(tf.zeros_like(tf.cast(c,"bool")))
-color = tf.Variable(tf.zeros_like(c, "int32"))
+def mandelbrotmenge():
 
+      #Maske aus Boolean Werte der Divergierte und Convergierten Pixels berechnen
+      ind = tf.abs(Z) < 2
 
-@tf.function
-def graphe(z,i,color):
-
-      #i.assign(i + 1)
-
-      # Convergierten Pixels berechnen
-      ind = tf.abs(z) < 2
-
-      # Convergierte Maske
+      #Convergierte Maske
       maskConvergent = tf.where(ind)
 
       #selectieren der convergierten 'z' Pixels der Iteration
-      zConvergent = tf.gather_nd(z, maskConvergent)
+      zConvergent = tf.gather_nd(Z, maskConvergent)
 
-      # selectieren der convergierten 'z' Pixels der Iteration
-      cConvergent = tf.gather_nd(c, maskConvergent)
+      # selectieren der zugehörigen 'c' Pixels der Iteration
+      cConvergent = tf.gather_nd(C, maskConvergent)
 
       #Berechnung der Mandelbrot-Menge
-      Z = zConvergent**2 + cConvergent
+      Zn = zConvergent**2 + cConvergent
 
       #Pixels aktualisieren und kopieren der neuen Maske in 'z'
-      z.assign(tf.tensor_scatter_nd_update(z, maskConvergent, Z))
+      Z.assign(tf.tensor_scatter_nd_update(Z, maskConvergent, Zn))
 
-      #die neuen divergierten Pixels
-      newDivergent = tf.equal(~ind, lastMask)
+      #Maske aus Boolean Werte der neu divergierten Pixels
+      newDivergent = tf.not_equal(ind, lastMask)
 
       #Zahl der Iteration reinschreiben
       color.assign(tf.where(newDivergent, i, color))
 
-      #Convergierten Pixels speichern
+      #Maske aus Boolean Werte der Divergierte und Convergierten Pixels speichern
       lastMask.assign(ind)
 
 
-for n in range(100):
+for n in range(10):
+    mandelbrotmenge()
     i.assign(i + 1)
-    graphe(z, i, color)
 
 plt.imshow(tf.abs(color))
 plt.show()
